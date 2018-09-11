73ea06389275d81332451372686ba25807be0d1b
Dennis Tse <dennis.tse@lalamove.com>
2018-09-11 16:31:22 +0800

    UPT: webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,9 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js',
   output: {
+    library: 'GameOfLife',
+    libraryTarget: 'window',
+    libraryExport: 'default',
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
